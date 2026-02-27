5f73d7b37b6a1ed728d7268428b24e2d6e20e161
Jean-D-alves <jenavinicius64@gmail.com>
2026-02-27 14:58:11 -0300

    message chat

--- js/Script.js
+++ js/Script.js
@@ -14,60 +14,69 @@ loginBtn.addEventListener("click", () => {
 
 function login() {
   const form = document.getElementById("form-login");
-  form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const email = document.getElementById("iemail").value;
-    const password = document.getElementById("ipass").value;
 
-    const response = await fetch(`${BackendUrl}/login/sing-in`, {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({ email: email, password: password }),
-    });
-    const data = await response.json();
-    if (response.ok) {
-      alert("login feito com sucesso");
-      setTimeout(() => {
-        window.location.href = "index.html";
-      }, 1500);
-    }
-    if (!response.ok) {
-      alert(data);
+  form.addEventListener("submit", async (e) => {
+    try {
+      e.preventDefault();
+      const email = document.getElementById("iemail").value;
+      const password = document.getElementById("ipass").value;
+
+      const response = await fetch(`${BackendUrl}/login/sing-in`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: email, password: password }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        alert("login feito com sucesso");
+        setTimeout(() => {
+          window.location.href = "Contact.html";
+        }, 1500);
+      }
+      if (!response.ok) {
+        alert(data.message || "Erro ao realizar login");
+      }
+    } catch (error) {
+      alert("Login error, please try again later.");
     }
   });
 }
 function register() {
   const form = document.getElementById("form-register");
   form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const name = document.getElementById("irname").value;
-    const email = document.getElementById("iremail").value;
-    const password = document.getElementById("irpass").value;
+    try {
+      e.preventDefault();
+      const name = document.getElementById("irname").value;
+      const email = document.getElementById("iremail").value;
+      const password = document.getElementById("irpass").value;
 
-    const response = await fetch(`${BackendUrl}/login/register`, {
-      method: "POST",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name: name,
-        email: email,
-        password: password,
-      }),
-    });
-    const data = await response.json();
-    if (response.ok) {
-      alert("register feito com sucesso");
-      setTimeout(() => {
-        window.location.href = "index.html";
-      }, 1500);
-    }
-    if (!response.ok) {
-      alert(data);
+      const response = await fetch(`${BackendUrl}/login/register`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          name: name,
+          email: email,
+          password: password,
+        }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        alert("register feito com sucesso");
+        setTimeout(() => {
+          window.location.href = "index.html";
+        }, 1500);
+      }
+      if (!response.ok) {
+        alert(data);
+      }
+    } catch (error) {
+      alert("Register error, please try again later.");
     }
   });
 }
